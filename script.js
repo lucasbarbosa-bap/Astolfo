92ba9e7f7f7af2619117234022c73d1f35c10bc3
Lucas 'lutaw <lucasbarbosa81612@gmail.comm>
2026-03-10 22:57:35 -0300

    Página principal de post deliciosos do Astolfo pronta

---
+++ script.js
@@ -0,0 +1,51 @@
+let PaginaAtual = 0;
+
+async function BuscarFemboys() {
+    console.log("Testando femboys");
+
+    const RespostaApi = await fetch(`https://corsproxy.io/?https://safebooru.org/index.php?page=dapi&s=post&q=index&json=1&limit=6&tags=astolfo_(fate)&pid=${PaginaAtual}`);
+
+    const dados = await RespostaApi.json();
+
+    console.log(dados);
+    TrocarFemboys(dados);
+}
+
+function TrocarFemboys(dados) {
+    const Imagem = document.querySelectorAll(".Galeria_project_chika figure img");
+
+    for(let i = 0; i <6; i++) {
+        const LinksFemboys = dados[i].file_url;
+        console.log("Femboys grátis em: ");
+        console.log(LinksFemboys);
+
+        Imagem[i].src = LinksFemboys;
+    }
+}
+
+
+const btv = document.getElementById("BtVoltar");
+const bta = document.getElementById("BtAvancar");
+
+btv.disabled = true
+
+function VoltarPagina(){
+    PaginaAtual -= 1
+    bta.disabled = false
+
+    if (PaginaAtual == 0) {btv.disabled = true};
+    BuscarFemboys()
+}
+
+function AvancarPagina(){
+    PaginaAtual += 1
+    btv.disabled = false
+
+    //if (PaginaAtual == 4) {bta.disabled = true};
+    BuscarFemboys() 
+}
+
+document.getElementById("BtVoltar").addEventListener("click", VoltarPagina)
+document.getElementById("BtAvancar").addEventListener("click", AvancarPagina)
+BuscarFemboys()
+
