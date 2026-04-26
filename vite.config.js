201f4a8571a236b37dece4831f5385e51a8e3acc
Lucas 'lutaw' Barbosa <lucasbarbosa81612@gmail.com>
2026-04-26 07:15:19 -0300

    erro do localhost (sim eu consegui fazer o site funcionar no servidor e n funcionar na minha maquina kkkk) n conseguir fazer o fetch arrumado

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: { '/api/safebooru': {
+      target: "https://safebooru.org",
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api\/safebooru/, '/index.php')
+
+    }},
+  },
   plugins: [
     vue(),
     vueDevTools(),
