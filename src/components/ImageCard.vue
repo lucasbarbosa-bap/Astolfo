d1b24a7a572ff7ad0a3b9d5adc16a9a3ee80546b
Lucas 'lutaw' Barbosa <lucasbarbosa81612@gmail.com>
2026-04-26 05:00:36 -0300

    Fetch da api, criação das figures e minha Mikan e Ibuki como placeholders 🩷 😊

---
+++ src/components/ImageCard.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(['urlFemboy'])
+</script>
+
+<template>
+    <figure>
+        <img src="/Ibuki_Mioda_Halfbody_Sprite_29.png" alt="Tela de acerto do programa mostrando o feedback visual com a personagem ibuki mioda" >
+        <!--<figcaption>A interface principal funcionando! Quando você acerta, a barra de combo rosa enche e a Ibuki aparece pra dar aquela incentivada.</figcaption>-->
+    </figure>
+
+    <figure>
+        <img src="/mikan_sprite.png" alt="Tela de acerto do programa mostrando o feedback visual com a personagem ibuki mioda" >
+        <!--<figcaption>A interface principal funcionando! Quando você acerta, a barra de combo rosa enche e a Ibuki aparece pra dar aquela incentivada.</figcaption>-->
+    </figure>
+</template>
--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,11 +1,40 @@
 <script setup>
-import AppHeader from '@/components/AppHeader.vue';
-import AppFooter from '@/components/AppFooter.vue';
+import ImageCard from '@/components/ImageCard.vue';
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+
+const gavetaFemboys = ref([])
+
+const PaginaAtual = 1
+
+async function BuscarFemboys() {
+    console.log("Testando femboys");
+
+    const RespostaApi = await fetch(`https://corsproxy.io/?https://safebooru.org/index.php?page=dapi&s=post&q=index&json=1&limit=6&tags=${route.query.tag}&pid=${PaginaAtual}`);
+
+    const dados = await RespostaApi.json();
+
+    console.log(dados);
+    gavetaFemboys.value = dados
+
+    console.log("Femboys Prontos!");
+    console.log(gavetaFemboys.value[0]);
+}
+
+onMounted(() =>
+    BuscarFemboys()
+)
 
 </script>
 
 <template>
     <main>
         <h2>Galeria de deliciosidades: </h2>
+        
+        <div class="Galeria_project_chika">
+            <ImageCard v-for="femboy in gavetaFemboys" :key="femboy.id" :urlFemboy="femboy.file_url" />
+        </div>
     </main>
 </template>
