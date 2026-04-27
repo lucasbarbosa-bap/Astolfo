d3b8ee03ca9a3591b231b3e45e167634a8fc1664
Lucas 'lutaw' Barbosa <lucasbarbosa81612@gmail.com>
2026-04-26 22:28:30 -0300

    Dropdown de sugestoes finalizado, agora podemos pesquisar qualquer femboy sem se importar com a tag exata

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,6 +6,7 @@ const router = useRouter()
 const route = useRoute()
 
 const tagAtual = ref("")
+const listaSugestoes = ref([])
 
 function fazerPesquisa() {
     console.log("Pesquisando por:", tagAtual.value);
@@ -13,6 +14,31 @@ function fazerPesquisa() {
     router.push({ path:'/post', query: {tag: tagAtual.value}})
 
     tagAtual.value = ""
+}
+
+async function buscarSugestoes() {
+    if(tagAtual.value.length > 2) {
+        const apiSugestoes = await fetch(`/api/safebooru?page=dapi&s=tag&q=index&json=1&limit=5&name_pattern=${tagAtual.value}%`)
+        const sugestoesXml = await apiSugestoes.text()
+
+        const parser = new DOMParser()
+        const xmlDoc = parser.parseFromString(sugestoesXml, "text/xml")
+
+        const tagsRetornadas = xmlDoc.getElementsByTagName("tag")
+
+        listaSugestoes.value = Array.from(tagsRetornadas).map(tag => tag.getAttribute("name"))
+
+        console.log("Procurando a merda da sugestão...")
+    } else {
+        listaSugestoes.value = []
+    }
+    
+}
+
+function selecionarSugestao(femboySugerido) {
+    tagAtual.value = femboySugerido
+
+    listaSugestoes.value = []
 }
 
 </script>
@@ -34,7 +60,10 @@ function fazerPesquisa() {
         </nav>
         
         <search>
-            <input type="text" v-model="tagAtual" placeholder="Digite a Tag (Ex: illyasviel_von_einzbern )">
+            <input type="text" v-model="tagAtual" @input="buscarSugestoes" placeholder="Digite a Tag (Ex: illyasviel_von_einzbern )">
+            <div v-if="listaSugestoes.length > 0" class="dropdownPesquisas">
+                <p v-for="sugestao in listaSugestoes" @click="selecionarSugestao(sugestao)">{{ sugestao }}</p>
+            </div>
             <button type="button" @click="fazerPesquisa">Search</button>
         </search>
             
