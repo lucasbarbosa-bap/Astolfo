cd6f5f986f929448c4613c848a519e25486a3996
Lucas 'lutaw' Barbosa <lucasbarbosa81612@gmail.com>
2026-05-03 08:03:44 -0300

    Imagem Modal de teste com a ibuki funcionando na pagina home

---
+++ src/components/ImageModal.vue
@@ -0,0 +1,37 @@
+<script setup>
+defineProps('')
+
+const emit = defineEmits(['fechar'])
+</script>
+
+<template>
+    <figure>
+        <button @click="() => {emit('fechar')}">X</button>
+        <img src="/Ibuki_Mioda_Halfbody_Sprite_29.png" alt="">
+    </figure>
+</template>
+
+<style scoped>
+    button{
+        display: flex;
+        margin-left: auto;
+    }
+    figure{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        z-index: 999;
+
+        width: 100vh;
+        height: 100vh;
+        background-color: aqua;
+    }
+
+    figure img{
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,23 @@
 <script setup>
 import AppFooter from '@/components/AppFooter.vue';
 import AppHeader from '@/components/AppHeader.vue';
+import ImageModal from '@/components/ImageModal.vue';
+import { ref } from 'vue';
+
+
+const mostrar = ref(false)
+
+function mostrarIbuki() {
+  mostrar.value = !mostrar.value
+
+  console.log(mostrar.value)
+}
 </script>
 
 <template>
   <main>
     <h2>Home teste</h2>
+    <button @click="mostrarIbuki"> IBUKI MIODA</button>
+    <ImageModal v-if="mostrar === true" @fechar="mostrar = false" />
   </main>
 </template>
